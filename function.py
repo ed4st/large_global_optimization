9a2cf29c47dc389ca9f60cb46f6b592a937277e9
Jorge Vargas <jorgelvb17@gmail.com>
2021-06-01 12:18:50 -0500

    elliptic, rastrigin, schwefel

--- function.py
+++ function.py
@@ -18,19 +18,29 @@ class Benchmark():
             return self.__rosenbrock
         else:
             return self.__sphere
-        
+    
+
     def __sphere(self, x):
         return np.dot(x,x)
     
     def __elliptic(self, x):
-        pass
+        D=x.shape[0]
+        sq_x=np.square(x)
+        t=np.array([np.power(10,6*(i-1)/(D-1)) for i in range(1,D+1)])
+        return np.dot(t,sq_x)
+
     def __rastringin(self, x):
-        pass
+        D=x.shape[0]
+        sq_x=np.square(x)
+        cosx=np.math.cos(2*np.pi*x)
+        return (np.sum(sq_x)+np.sum(cosx)+D*10)
     def __ackley(self, x):
         D = len(x)
         return -20*np.exp(-.2*np.sqrt((1/D)*np.dot(x,x)) - np.exp((1/D)*sum(np.cos(2*np.pi*x)))) + 20 +np.e
     def __schwefel(self, x):
-        pass
+        csum=np.cumsum(x,axis=1)
+        sq_csum=np.square(csum)
+        return np.sum(sq_csum)
     def __rosenbrock(self, x):
         D = len(x)
         return sum([100*(x[i]**2- x[i+1])**2 + (x[i] - 1)**2 for i in range(D-1)] )
